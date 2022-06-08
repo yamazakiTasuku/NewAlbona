beadb8d231c8e1b2d73e775f74e4916630bcb26d
yamazakiTasuku <yamazaki4193161411@gmail.com>
2022-06-08 17:22:29 +0900

    URL保存

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,37 +1,48 @@
-import { useState } from "react";
+import { useState} from "react";
 import AnserList from "../AnserList"
 import Link from 'next/link'
 import {useRouter} from "next/router"
 import  {QuestionItemGive} from "./questionItem"
-import * as fs from 'fs';
-
-const App = () => {
+  
+const App  = () => {
     const [truechecked,setruecheckedt] = useState([{Nchecked:Boolean,Uchecke:Boolean,Cchecked:Boolean,id:Number}]);
     const [allchecker,setallchecker] = useState([])
-    let UrlNum = 0
-    let url,urlStr:string
+    const [nextUrl,setnextUrl] = useState(String)
+
     const router = useRouter();
     let {id} = router.query;
-    urlStr = `${id}`
-    UrlNum = Number(urlStr.slice(-1)) + 1
-    let anserLists = QuestionItemGive(Number(urlStr.slice(-1)))
-    type anserConterType={
-        NetflixNum:number;
-        UNextNum:number;
-        DisneyPlusNum:number;
-    }
-    fs.writeFileSync('a.txt', 'b')
-    
+    const urlStr = `${id}`
+    const UrlNum = Number(urlStr.slice(-1)) + 1
+    const anserLists = QuestionItemGive(Number(urlStr.slice(-1)))
+    const url = "/product/Question"  
+
     const toggleAnser = (counter) =>{
         const newtruechecked = truechecked.filter((checked) => checked.id != counter[0].id)
         setruecheckedt([...newtruechecked,...counter]);
         const Rnewtruechecked = [...newtruechecked,...counter]
-        setallchecker([Rnewtruechecked.filter((checked)=> checked.Nchecked===false).length,Rnewtruechecked.filter((checked)=> checked.Uchecked===false).length,Rnewtruechecked.filter((checked)=> checked.Cchecked===false).length])
-    }    
-    
 
-    url = "/product/Question"  + String(UrlNum)
-    let classN = "flex justify-center my-10"
+        const treuchecker =[Rnewtruechecked.filter((checked)=> checked.Nchecked===false).length,Rnewtruechecked.filter((checked)=> checked.Uchecked===false).length,Rnewtruechecked.filter((checked)=> checked.Cchecked===false).length]
+        setallchecker([Rnewtruechecked.filter((checked)=> checked.Nchecked===false).length,Rnewtruechecked.filter((checked)=> checked.Uchecked===false).length,Rnewtruechecked.filter((checked)=> checked.Cchecked===false).length])
+        //console.log(treuchecker)
+        if(Rnewtruechecked.length === 7){
+            //console.log(allchecker)
+            console.log(Rnewtruechecked)
+            if(UrlNum == 2){
+                setnextUrl(url +  String(treuchecker[0]) + String(treuchecker[1]) + String(treuchecker[2]) + String(UrlNum))
+            }else if(UrlNum > 2){
+                const Url1Num = Number(urlStr.slice(-1))
+                const Url2Num = Number(urlStr.slice(-2))
+                const Url3Num = Number(urlStr.slice(-3))
+                setnextUrl(urlStr +  String(treuchecker[0]) + String(treuchecker[1]) + String(treuchecker[2]) + String(UrlNum))
+            }
+        }
+    }    
+
+    const NextPage=()=>{
+        //console.log(url)
+    }
+
+    const classN = "flex justify-center my-10"
     return(
         <div>
             <div className="flex justify-center">
@@ -44,8 +55,8 @@ const App = () => {
                 <AnserList anserList={anserList} toggleAnser={toggleAnser} key={anserList.Netflix} Id={anserList.id}/>
             ))}
             <div className={classN}>
-                <Link href={url}>
-                    <button className='mt-7 text-3xl px-20 py-10 bg-blue-400 text-white font-semibold rounded hover:bg-blue-500'>
+                <Link href={nextUrl} >
+                    <button onClick={()=>NextPage()} className='mt-7 text-3xl px-20 py-10 bg-blue-400 text-white font-semibold rounded hover:bg-blue-500'>
                     次の質問へ!!
                     </button>
                 </Link>
